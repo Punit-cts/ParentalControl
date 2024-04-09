c8e4b6c8357cbe87d71bfaecbacbe7395cfbb30c
Punit Thakali <punit@cloudtechservice.com>
2024-04-09 06:34:34 +0545

    project file

---
+++ ParentalControl/Authentication/UserSelection/UserParentSelectionView.swift
@@ -0,0 +1,20 @@
+//
+//  UserParentSelectionView.swift
+//  ParentalControl
+//
+//  Created by Punit Thakali on 09/04/2024.
+//
+
+import UIKit
+
+class UserParentSelectionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ParentalControl/Authentication/View/LoginNetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  LoginNetworkManage.swift
+//  ParentalControl
+//
+//  Created by Punit Thakali on 09/04/2024.
+//
+
+import Foundation
